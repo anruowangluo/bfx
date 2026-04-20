11f307462fd19de4d28de1bd2aba175707d40206
anruowangluo <anruowangluo@users.noreply.github.com>
2026-04-20 07:58:11 +0000

    feat: 创建移动端提示词分享网站
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, useLocation, useEffect } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import PromptDetail from './pages/PromptDetail'
 import CreatePrompt from './pages/CreatePrompt'
